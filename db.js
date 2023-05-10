3108c3c6d23d07f49b97692def924bf4661ea8d2
Akshay Aileni <85357123+Akxai@users.noreply.github.com>
2023-05-10 08:07:51 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    username: "admin",
+    password: "admin",
+  },
+  {
+    username: "user",
+    password: "helloworld",
+  },
+  {
+    username: "Akshay",
+    password: "helloworld",
+  },
+];
